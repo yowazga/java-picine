2cfa2b12c626d126ce9eff7a8f920cf5e76fe014
Younes Wazga <yowazga@c2r13p6.1337.ma>
2024-07-02 16:33:08 +0100

    second

---
+++ module-00/ex02/Program.java
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Program.java                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yowazga <yowazga@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/01 19:05:14 by yowazga           #+#    #+#             */
+/*   Updated: 2024/07/02 14:26:26 by yowazga          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// package module-00.ex02;
+import java.util.Scanner;
+
+public class Program {
+	boolean isPrime(int num)
+	{
+		if (num == 2 || num == 3)
+			return true;
+		if (num % 2 == 0 || num % 3 == 0)
+			return false;
+		for (int i = 5; i * i <= num; i += 6)
+		{
+			if (num % i == 0 || num % (i + 2) == 0)
+				return false;
+		}
+		return true;
+	}
+
+	int sumOfDigits(int number)
+	{
+		int sum = 0;
+
+		while (number != 0)
+		{
+			sum += number % 10;
+			number /= 10;
+		}
+		return sum;
+	}
+	
+	public static void main(String[] args) {
+
+		Program program = new Program();
+		Scanner input = new Scanner(System.in);
+		int count = 0;
+		int num = 0; 
+
+		while (true) {
+			num = input.nextInt();
+			if (num == 42)
+				break ;
+			if (program.isPrime(program.sumOfDigits(num)))
+				count++;
+		}
+		System.out.println("Count of coffee-request : " + count);
+		input.close();
+	}
+}
