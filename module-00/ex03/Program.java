8d2ec0e8de9a57b5564a74846c243d12635315cf
Younes Wazga <yowazga@c1r8p8.1337.ma>
2024-08-27 20:38:24 +0100

    finish ex03 modell-00

--- module-00/ex03/Program.java
+++ module-00/ex03/Program.java
@@ -1,19 +1,85 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   Program.java                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: yowazga <yowazga@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/02 15:42:18 by yowazga           #+#    #+#             */
-/*   Updated: 2024/07/02 18:19:30 by yowazga          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 import java.util.Scanner;
 
 public class Program {
-	public static void main(String[] args) {
-		
+
+	private static Scanner scanner = new Scanner(System.in);
+	private static int weeks = 0;
+
+	private static int getMinGrad()
+	{
+		int minGrage = 9;
+		for (int i = 0; i < 5; i++)
+		{
+			if (scanner.hasNextInt())
+			{
+				int grade = scanner.nextInt();
+				if (grade < 1 || grade > 9)
+				{
+					System.err.println("IllegalArgument");
+					scanner.close();
+					System.exit(-1);
+				}
+				minGrage = grade < minGrage ? grade : minGrage;
+			}
+			else
+			{
+				System.err.println("IllegalArgument");
+				scanner.close();
+				System.exit(-1);
+			}
+		}
+		scanner.nextLine();
+		return minGrage;
+	}
+
+	private static long getWeekGrades()
+	{
+		long grades = 0;
+		String input = scanner.nextLine();
+
+		while (weeks < 18 && !input.equals("42"))
+		{
+			if (input.equals("Week " + (weeks + 1)))
+			{
+				grades = grades * 10 + getMinGrad();
+				weeks++;
+				input = scanner.nextLine();
+			}
+			else
+			{
+				System.err.println("IllegalArgument");
+				scanner.close();
+				System.exit(-1);
+			}
+		}
+		scanner.close();
+		return grades;
+	}
+
+    private static void printGraph(long grades)
+	{
+        long divisor = 1;
+
+        for (int i = 1; i < weeks; i++) {
+            divisor *= 10;
+			System.out.println("divisor is: " + divisor);
+
+        }
+
+        for (int i = 0; i < weeks; i++) {
+            int minGrade = (int) (grades / divisor) % 10;
+            System.out.print("Week " + (i + 1) + " ");
+            for (int j = 0; j < minGrade; j++) {
+                System.out.print("=");
+            }
+            System.out.println(">");
+            divisor /= 10;
+        }
+    }
+
+	public static void main(String[] args)
+	{
+		long grades = getWeekGrades();
+		printGraph(grades);
 	}
 }
