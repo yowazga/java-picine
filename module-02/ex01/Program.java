44168c90e5bdf6001b49f6ef6c3741af4afe0a40
Younes Wazga <yowazga@c1r8p8.1337.ma>
2024-10-18 16:45:18 +0100

    add ex02-mod02

--- module-02/ex01/Program.java
+++ module-02/ex01/Program.java
@@ -6,7 +6,7 @@
 /*   By: yowazga <yowazga@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 16:48:21 by yowazga           #+#    #+#             */
-/*   Updated: 2024/10/16 15:24:25 by yowazga          ###   ########.fr       */
+/*   Updated: 2024/10/16 15:38:31 by yowazga          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -115,7 +115,7 @@ public class Program {
 
 		writeDictionary(dictionary, "dictionary.txt");
 
-		System.out.println(similarity);
+		System.out.printf("Cosine similarity: %.4f\n", similarity);
 
 	}
 }
---
+++ module-02/ex02/Program.java
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Program.java                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yowazga <yowazga@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/17 12:10:42 by yowazga           #+#    #+#             */
+/*   Updated: 2024/10/17 13:46:24 by yowazga          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+import java.nio.file.Paths;
+import java.nio.file.Path;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Path inputPaht = Paths.get(args[0]);
+	}
+}
