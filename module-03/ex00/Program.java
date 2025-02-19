bf0f7112d5cd2556c1f927af34bed54161654b28
Younes wazga <younes@Youness-MacBook-Pro.local>
2025-02-19 12:13:39 +0100

    commit rabat

--- module-03/ex00/Program.java
+++ module-03/ex00/Program.java
@@ -6,78 +6,14 @@
 /*   By: yowazga <yowazga@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 12:35:45 by yowazga           #+#    #+#             */
-/*   Updated: 2024/11/29 09:49:52 by yowazga          ###   ########.fr       */
+/*   Updated: 2025/02/12 11:27:23 by yowazga          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 public class Program {
-
-	static void ThreadMessage(String message) {
+	public static void main(String[] args)
+	{
+		MyThread thread1 = new MyThread();
 		
-		String threadName = Thread.currentThread().getName();
-		System.out.format("%s: %s%n", threadName, message);
-	}
-
-	private static class MessageLoop implements Runnable {
-		
-		@Override
-		public void run() {
-			String importantInfo[] = {
-				"Mares eat oats",
-				"Does eat oats",
-				"Little lambs eat ivy",
-				"A kid will eat evy too"
-			};
-			try {
-				for (int i = 0; i < importantInfo.length; i++) {
-					
-					Thread.sleep(4000);
-					
-					ThreadMessage(importantInfo[i]);
-				}
-				
-			} catch (InterruptedException e) {
-				ThreadMessage("I wasn't done!");
-			}
-		}
-	}
-	
-	
-	public static void main(String[] args) throws InterruptedException{
-
-		long patience = 1000 * 60 * 60;
-
-		if (args.length > 0) {
-			
-			try {
-				patience = Long.parseLong(args[0]) * 1000;
-			} catch (NumberFormatException e) {
-				System.err.println("argument must be an integer.");
-				System.exit(1);
-			}
-		}
-
-		ThreadMessage("Starting MessageLoop thread");
-		
-		long startTime = System.currentTimeMillis();
-		Thread t = new Thread(new MessageLoop());
-		t.start();
-
-		ThreadMessage("Waiting for MessageLooop thread to finish");
-		
-		while (t.isAlive()) {
-			
-			ThreadMessage("Still waiting...");
-			
-			t.join(1000);
-
-			if (((System.currentTimeMillis() - startTime) > patience) && t.isAlive()) {
-				
-				ThreadMessage("Tired of waiting!");
-				t.interrupt();
-				t.join();
-			}
-		}
-		ThreadMessage("Finally!");
 	}
 }
