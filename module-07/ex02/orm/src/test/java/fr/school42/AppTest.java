83fdbf86d91d085addeb8b38b76de104cc37a029
younes wazga <Younes@Mehdi.local>
2025-06-23 12:02:59 +0100

    finish ex01 mod07

---
+++ module-07/ex02/orm/src/test/java/fr/school42/AppTest.java
@@ -0,0 +1,20 @@
+package fr.school42;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
