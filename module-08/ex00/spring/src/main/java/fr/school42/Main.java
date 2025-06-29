b01fc8f454fd1ac5e40fdd81ea3fe04c9c41715d
younes wazga <Younes@Mehdi.local>
2025-06-29 14:22:08 +0100

    add module-09

--- module-08/ex00/spring/src/main/java/fr/school42/Main.java
+++ module-08/ex00/spring/src/main/java/fr/school42/Main.java
@@ -6,7 +6,7 @@
 /*   By: Younes <Younes@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/25 16:28:11 by Younes            #+#    #+#             */
-/*   Updated: 2025/06/25 19:18:10 by Younes           ###   ########.fr       */
+/*   Updated: 2025/06/25 19:46:16 by Younes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ public class Main
     {
         ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
 
-        Printer print = context.getBean("printerWithDate", Printer.class);
+        Printer print = context.getBean("printerWithPrefix", Printer.class);
         print.print("hello");
     }
 }
--- module-08/ex01/service/src/main/java/school42/spring/service/application/Main.java
+++ module-08/ex01/service/src/main/java/school42/spring/service/application/Main.java
@@ -6,36 +6,30 @@
 /*   By: Younes <Younes@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/26 14:42:26 by Younes            #+#    #+#             */
-/*   Updated: 2025/06/27 18:40:00 by Younes           ###   ########.fr       */
+/*   Updated: 2025/06/28 14:41:24 by Younes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 package school42.spring.service.application;
 
-import java.nio.file.OpenOption;
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import school42.spring.service.models.User;
 import school42.spring.service.repositories.UsersRepository;
 import school42.spring.service.repositories.UsersRepositoryJdbcImpl;
+import school42.spring.service.repositories.UsersRepositoryJdbcTemplateImpl;
 
 public class Main 
 {
     public static void main( String[] args ) {
+        try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml")) {
+            
+            UsersRepository repo1 = context.getBean("usersRepositoryJdbc", UsersRepositoryJdbcImpl.class);
+            System.out.println("=== JDBC Impl ===");
+            repo1.findAll().forEach(System.out::println);
     
-        ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
-
-        UsersRepository usersRepository = context.getBean("usersRepositoryJdbc", UsersRepositoryJdbcImpl.class);
-        
-        Optional<User> optional = usersRepository.findByEmail("zakdaria@42.fr");
-        
-        System.out.println(optional.get());
-        
-        
+            UsersRepository repo2 = context.getBean("UsersRepositoryJdbcTemplateImpl", UsersRepositoryJdbcTemplateImpl.class);
+            System.out.println("=== JdbcTemplate Impl ===");
+            repo2.findAll().forEach(System.out::println);
+        }
         
     }
 }
